15b707430b73523980c8eab6f9c2d37d2831ef52
unknown <2137516@labinfo.is.escuelaing.edu.co>
2019-08-23 12:01:23 -0500

    Ejercicio 1 completo

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/URLReader.java
@@ -0,0 +1,28 @@
+package edu.escuelaing.arem.ASE.app;
+
+import java.io.*;
+import java.net.*;
+
+public class URLReader {
+
+	public static void main(String[] args) throws Exception {
+    
+		URL facebook = new URL("http://www.facebook.com/");
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(facebook.openStream()))) {
+			String inputLine = null;
+			while ((inputLine = reader.readLine()) != null) {
+				System.out.println(inputLine);
+			}			
+			System.out.println(facebook.getAuthority()+"\n");
+			System.out.println(facebook.getProtocol()+"\n");
+			System.out.println(facebook.getPath()+"\n");
+			System.out.println(facebook.getHost()+"\n");
+			System.out.println(facebook.getPort()+"\n");
+			System.out.println(facebook.getQuery()+"\n");
+			System.out.println(facebook.getFile()+"\n");
+			System.out.println(facebook.getRef()+"\n");
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+	}
+}
